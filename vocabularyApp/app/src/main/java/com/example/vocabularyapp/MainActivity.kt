3c11b44f231deffa6f733166ddaea3e61c0cef27
choi98-git <79885792+choi98-git@users.noreply.github.com>
2024-02-26 23:20:55 +0900

    wordNote
    UI

---
+++ vocabularyApp/app/src/main/java/com/example/vocabularyapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.vocabularyapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.vocabularyapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener{
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addButton.setOnClickListener {
+            val intent = Intent(this, AddActivity::class.java)
+            startActivity(intent)
+        }
+
+        initRecyclerView()
+    }
+
+
+    private fun initRecyclerView(){
+        val dummyList = mutableListOf(
+            Word("weather","날씨","명사"),
+            Word("honey","꿀","명사"),
+            Word("run","실행하다","동사")
+        )
+        wordAdapter = WordAdapter(dummyList,this)
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    override fun onClick(word: Word) {
+        Toast.makeText(this,"${word.text}가 클릭 됐습니다", Toast.LENGTH_SHORT).show()
+    }
+}
